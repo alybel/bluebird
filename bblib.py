e010d6760afb89379099098137d2c70c50dbfe7e
alybel <al.d.beck@gmail.com>
2014-04-24 23:48:18 +0200

    improved functionality on bblib

--- bblib.py
+++ bblib.py
@@ -1,34 +1,37 @@
 #Basic functionality used for the bluebird project"
 import tweepy
 import json
+import config as cfg
+import bbanalytics
 
-
-def load_config_file(config_file = "config"):
-    """
-    Load app specific credentials for the OAuth authentification
-    """
-    config = {}
-    with open(config_file,'r') as f:
-        for line in f:
-            key,val = line.strip("\n").split("=")
-            config[key] = val
-    q = set(config.keys())^set(["consumer_key","consumer_secret", "access_token", "access_token_secret"])
-    if len(q) == 0:
-        return config
-    else:
-        print "Error: Config file needs consumer_key, consumer_secret, access_token, access_token_secret"
-
-def connect_app_to_twitter(config_file = "config"):
+def connect_app_to_twitter():
     """
     Use credential in config file and use OAuth to connect to twitter. return the authentication and the api.
     """
-    cfg = load_config_file(config_file)
-    print cfg
-    auth = tweepy.OAuthHandler(cfg["consumer_key"], cfg["consumer_secret"])
-    auth.set_access_token(cfg["access_token"], cfg["access_token_secret"])
+    auth = tweepy.OAuthHandler(cfg.consumer_key, cfg.consumer_secret)
+    auth.set_access_token(cfg.access_token, cfg.access_token_secret)
     api = tweepy.API(auth)
     api.rate_limit_status()
     return auth, api
+
+def ru(s = ""):
+    """
+    resolve unicode and return printable string
+    """
+    return None if not s else s.encode('ascii', 'ignore')
+
+def get_first_level_content(data, key):
+    return None if not key in data else ru(data[key])
+
+def tweet2obj(data):
+    class t:pass
+    data = json.loads(data)
+    t.text = get_first_level_content(data,"text")
+    t.lan = get_first_level_content(data,"lang")
+    t.created = get_first_level_content(data,"created_at")
+    user = data["user"]
+    t.description = get_first_level_content(user,"description")
+    return t
 
 ###
 ###
@@ -38,21 +41,20 @@ def connect_app_to_twitter(config_file = "config"):
 
 class DummyListener(tweepy.StreamListener):
     def on_data(self, data):
-        d = json.loads(data)
-        print d["text"]
+        tweet = tweet2obj(data)
+        print tweet.text
+        print tweet.created
         return True
     def on_error(self, status):
         print "error: ",
         print status
 
 def test_stream():
-    auth, api = connect_app_to_twitter("config")
+    auth, api = connect_app_to_twitter()
     l = DummyListener()
     stream = tweepy.Stream(auth, l)
-    stream.filter(track=['analytics','data'])
+    stream.filter(track=cfg.keywords)
     
-
 if __name__ == '__main__':
-    load_config_file()
     connect_app_to_twitter()
     test_stream()
