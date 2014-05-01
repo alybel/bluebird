a59e0ebf4e01fbc1d81b88b0bbbe17ed7fffe3ce
alybel <al.d.beck@gmail.com>
2014-05-01 17:13:19 +0200

    updated config template

--- config_template.py
+++ config_template.py
@@ -1,13 +1,17 @@
-consumer_key="XXXXXXX"
-consumer_secret="XXXXXXXXX"
-access_token="XXXXXXXXX"
-access_token_secret="XXXXXXXXXXX"
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_token_secret=""
 
-keywords = ["twitter", "news", "politcs"]
+own_twittername = "AlexanderD_Beck"
 
-own_twittername = "jbond"
+keywords = ["predictive","analytics","datascience","predictiveanalytics","startup",
+            "datascientist","knime","dataanalytics","data","visualization","datavisualization",
+            "ecommerce", "entrepreneur", "entrepreneurship", "designthinking", "predictiveanalytics", 
+            "leanstartup", "founder", "datamining", "python", "scikit", "machinelearning", "rstudio", "spss", "sas", "business", 
+            "invest", "ceo", "cio", "prescriptive", "visualization", "business"]
 
-negative_keywords = ["price", "shop", "advertising", "marketing", "music", "buy"]
+negative_keywords = ["price", "shop", "advertising", "ebay", "marketing", "music", "buy", "free", "only", "cheap", "guarantee", "birthday", "job", "offer"]
 
 languages = ["de", "en"]
 locations = []
@@ -18,7 +22,17 @@ only_with_url = True
 
 number_active_favorites = 300
 number_active_retweets = 300
-number_active_follows = 300
+number_active_follows = 1000
 
+#the higher the score the more exact do tweets match your needs but less tweets per time unit will be considered
+favorite_score = 2
+retweet_score = 2
+follow_score = 2
+dump_score = 2
+            
 #Set the maximum number of hastags you would like to favorite in a tweet. Set -1 to ignore this option.
 number_hashtags = 3
+
+accounts_never_delete = ["SMEhubb", "BYAnalytics", "WeissU", "hmason", "todd_park" ,"SethGrimes", "biscorecard",
+                         "kdnuggets", "eric_kavanagh", "HKotadia", "marcusborba", "TheGrok", "usamaf", "data_nerd",
+                         "revodavid", "aneeshchopra", "kirkdborne" , "datagenius", "bigdatagal", "yvesmulkers", "MGoldbergData", "johnlucker"]
